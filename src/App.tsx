bdec89f011b9c92fe2081ea14bc44a3f6fcdafb6
RomaMatsen <marock26011981@gmail.com>
2025-01-18 16:56:49 +0100

    slkgbelrbj

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import "./App.css";
-import React from "react";
+import {useState} from "react";
 
 function App() {
-    const [items, setItems] = React.useState(["Item 1"]);
+    const [items, setItems] = useState(["Item 1"]);
+    const [title, setTitle] = useState("");
 
     const handleClick = () => {
-        const newItems = [...items, "Ne item"];
+        const newItems = [...items, title];
         setItems(newItems);
+        setTitle("");
     };
 
     return (
-        <div>
-            <h1>Simple todo list</h1>
-            <div>{items}</div>
-            <button onClick={handleClick} className="p-2 rounded-full px-4 bg-slate-300">Click me</button>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div className="p-5 rounded-xl gap-5 bg-slate-100 flex flex-col border-2 border-solid border-state-300">
+            <h1 className="text-3xl font-medium"> Simple todo list</h1>
+            <div className="flex flex-col gap-2">{
+              items.map((item, index) => {
+                return (
+                  <div key={index} className="p-2 rounded-full bg-slate-200 text-black">
+                    {item}
+                  </div>
+                )
+              }
+              )
+              }</div>
+            <input type="text" value={title}
+            className="rounded-full p-1 bg-white text-black border border-solid border-slate-300 px-2"
+            onChange={(e)=> {setTitle(e.currentTarget.value);}}
+            ></input>
+            <button onClick={handleClick} className="p-2 rounded-full px-4 bg-slate-300">add me</button>
         </div>
+      </div>
     );
 }
 
