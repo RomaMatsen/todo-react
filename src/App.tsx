9270e5e8179215d6d140a5945c39f600b291d7e3
Denis Matsenko <matsenkodenis0@gmail.com>
2025-01-18 14:21:54 +0100

    Added tailwind tags

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  const [items, setItems] = React.useState(["Item 1"]);
+    const [items, setItems] = React.useState(["Item 1"]);
 
-  const handleClick = () => {
-    const newItems = [...items, "Ne item"];
-    setItems(newItems);
-  };
+    const handleClick = () => {
+        const newItems = [...items, "Ne item"];
+        setItems(newItems);
+    };
 
-  return (
-    <div>
-      <h1>Simple todo list</h1>
-      <div>{items}</div>
-      <button onClick={handleClick}>Click me</button>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Simple todo list</h1>
+            <div>{items}</div>
+            <button onClick={handleClick} className="p-2 rounded-full px-4 bg-slate-300">Click me</button>
+        </div>
+    );
 }
 
 export default App;
