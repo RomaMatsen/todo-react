6c77a4e3046b7beb5e8656bf26e42b1c4dd35fae
RomaMatsen <marock26011981@gmail.com>
2025-01-19 17:27:02 +0100

    not finished navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
 import "./App.css";
 import { useState } from "react";
 import { Todo } from "./components/todo";
+import { NavItemBuild } from "./components/nav";
 import { TodoItemInterface } from "./types";
 
 function App() {
     const [items, setItems] = useState<TodoItemInterface[]>([]);
     const [title, setTitle] = useState<string>("");
-
-    const handleClick = () => {
-        const newItems: TodoItemInterface[] = [...items,
-        {
-            title: title,
-            completed: false,
-        }
-        ];
+    const addTodoItem = () => {
+        const newItems: TodoItemInterface[] = [...items, {title: title, completed: false,}];
         setItems(newItems);
         setTitle("");
     };
-
     const doneItem = (index: number) => {
         const newItems: TodoItemInterface[] = [...items];
         newItems[index].completed = !newItems[index].completed;
         setItems(newItems);
     }
+    const [navItems, setNavItems] = useState(["Todo list", "Contacts", "About"]);
+    const [pageNumber, setPageNumber] = useState(1);
+    const openPage = (index: number) => {
+
+    }
 
     return (
-        <div className="w-screen h-screen flex justify-center items-center">
+        <div className="w-screen h-screen flex flex-col">
+          <div className="w-screen flex justify-end">
+            {
+              navItems.map((navItem, index)=><NavItemBuild key={index} str={navItem} pageNumber={pageNumber} openPage={() => {openPage (pageNumber)}}/>)
+            }
+          </div>
+          <div className="flex justify-center h-screen">
             <div className="p-5 rounded-xl gap-5 bg-slate-100 flex flex-col border-2 border-solid border-state-300">
                 <h1 className="text-3xl font-medium"> Simple todo list</h1>
                 <div className="flex flex-col gap-2">
                     {
-                        items.map((ArrItem, i) =>
-                            <Todo key={i} item={ArrItem} doneItem={() => { doneItem(i) }} />
-                        )
+                        items.map((ArrItem, i) => <Todo key={i} item={ArrItem} doneItem={() => { doneItem(i) }}/>)
                     }
                 </div>
                 <input
@@ -41,8 +44,9 @@ function App() {
                     className="rounded-full p-1 bg-white text-black border border-solid border-slate-300 px-2"
                     onChange={(e) => { setTitle(e.currentTarget.value); }}
                 />
-                <button onClick={handleClick} className="p-2 rounded-full px-4 bg-slate-300">add me</button>
+                <button onClick={addTodoItem} className="p-2 rounded-full px-4 bg-slate-300">add</button>
             </div>
+          </div>
         </div>
     );
 }
--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -8,14 +8,9 @@ interface Prop {
 export const Todo = (p: Prop) => {
     return (
         <div className={`p-2 flex justify-between rounded-full text-black ${p.item.completed ? "bg-green-200" : "bg-slate-200"}`}>
-            <p>
-                {p.item.title}
-            </p>
-
+            <p> {p.item.title} </p>
             <button onClick={p.doneItem}>
-                {
-                    p.item.completed ? "undone" : "done"
-                }
+                {p.item.completed ? "undone" : "done"}
             </button>
         </div>
     )
